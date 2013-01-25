22037f34df4319823b55a8ed50318477e852240c
Grigory Fateyev <gfborn@gmail.com>
2013-01-25 18:43:48 +0400

    Add plot line for pressure from task 2.

--- weathe.R
+++ weathe.R
@@ -1,4 +1,4 @@
-yplot <- function(years){
+yplot <- function(years, pres = FALSE){
   ## Get data of the year and render plots
   
   par(mfrow = c(length(years), 1), mai = c(1.2, 1, 0.5, 1))
@@ -8,6 +8,7 @@ yplot <- function(years){
                      colClasses = "character", sep = ";")
     data[,1] <- as.POSIXct(data[,1], format = "%d.%m.%Y %H:%M")
     data[,2] <- as.numeric(data[,2])
+    data[,3] <- as.numeric(data[,3])
     
     nt <- data[grep("01:00", data[,1]),]
     dt <- data[grep("13:00", data[,1]),]
@@ -15,10 +16,11 @@ yplot <- function(years){
     plot(nt[,1], nt[,2], type = "l", col = "blue", ylim = c(-30, 40), xaxt = "n",
          main = sprintf("Temperate for %d Year", year),
          xlab = "", ylab = "Temperate Rank")
-    legend("topright", c("Night", "Day"), lty = c(1, 1),
-           col = c("blue", "red"))
+    legend("topright", c("Night", "Day", "Pressure"), lty = c(1, 1, 1),
+           col = c("blue", "red", "green"))
     points(dt[,1], dt[,2], type = "l", col = "red")
     
+    ## axis
     ms <- split(data, months(data[,1]))
     months <- names(ms)
     at <- c()
@@ -30,5 +32,14 @@ yplot <- function(years){
     axis(1, at = lapply(ms, function(x) x[nrow(x)/2,1]), 
          labels = sprintf("%s (%.1f)", bymonths[,1], round(bymonths[,2], 1)),
          las = 2, cex.axis = 0.8)
+    
+    ## pressure
+    if(pres) {
+      par(new = TRUE)
+      plot(data[,1], data[,3], type = "l", col = "green", ylab = "", xlab = "",
+           ylim = c(710, 780), axes = FALSE, xaxt = "n")
+      
+      axis(4, pretty(seq(710, 780, 5)), col = "green")
+    }
   }
 }
